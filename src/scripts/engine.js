516643b21d52468270387ced0d21f58f106f3c61
Charles Dennis <charlesdennis2009@hotmail.com>
2024-11-26 20:09:16 -0300

    Dando efeito de rotate nos cards

---
+++ src/scripts/engine.js
@@ -0,0 +1,47 @@
+const emojis =[
+    "😍",
+    "😍",
+    "😎",
+    "😎",
+    "✌",
+    "✌",
+    "🌹",
+    "🌹",
+    "🎉",
+    "🎉",
+    "🤷‍♀️",
+    "🤷‍♀️",
+    "🤢",
+    "🤢",
+    "😥",
+    "😥",
+    "🤣",
+    "🤣",
+];
+
+let openCards =[];
+
+let shuffleEmojis = emojis.sort(() =>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i< emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if(openCards === 2){
+        setTimeout(checkMatch, 500);
+
+    }
+}
+
+function checkMatch(){
+    
+}
